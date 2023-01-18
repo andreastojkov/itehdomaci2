90f007fedf18ceda7b03f8e89a82e0bb58ae77d1
Andrea Stojkov <andreastojkov00@gmail.com>
2023-01-18 21:17:40 +0100

    Definisan seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Provider;
+use \App\Models\Service;
+use \App\Models\AppointmentRating;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +18,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //brisanje svega iz tabela
+        User::truncate();
+        Provider::truncate();
+        Service::truncate();
+        AppointmentRating::truncate();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory(5)->create();
+        Provider::factory(10)->create();
+
+        $service1 = Service::create([
+            'name' => 'Ocni pregled'
+        ]);
+
+        $service2 = Service::create([
+            'name' => 'Dermatoloski pregled'
+        ]);
+
+        $service3 = Service::create([
+            'name' => 'Otorinolaringoloski pregled'
+        ]);
+
+        $service4 = Service::create([
+            'name' => 'Vadjenje krvi'
+        ]);
+
+        $service5 = Service::create([
+            'name' => 'Pregled za vozacku'
+        ]);
+
+        $appointementRating1 = AppointmentRating::create([
+            'date_and_time' => now(),
+            'user' => 1,
+            'service' => 1,
+            'provider' => 1,
+            'rating' => 5,
+            'note' => 'Posvecena doktorka, proverila mi je dioptriju i ocni pritisak. Sve pohvale.'
+        ]);
+
+        $appointementRating2 = AppointmentRating::create([
+            'date_and_time' => now(),
+            'user' => 2,
+            'service' => 1,
+            'provider' => 2,
+            'rating' => 5,
+            'note' => 'Resili su mi problem - oko me je peklo svaki dan. Dali su mi kapi koje sipam par dana. Posveceni, sve pohvale.'
+        ]);
     }
 }
