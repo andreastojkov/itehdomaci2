7257bd1a9b527af02466a0666e816dccff852317
Andrea Stojkov <andreastojkov00@gmail.com>
2023-01-20 00:06:06 +0100

    Skinut doctrine/dbal,sanctum,ispravljena greska

--- database/migrations/2023_01_18_194622_create_appointments_ratings_table.php
+++ database/migrations/2023_01_18_194622_create_appointments_ratings_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('appointment_ratings', function (Blueprint $table) {
+        Schema::create('appointments_ratings', function (Blueprint $table) {
             $table->id();
             $table->timestamp('date_and_time');
             $table->foreignId('user');
